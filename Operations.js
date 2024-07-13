79ed6b46706920025cac830c8f2e3d4205c94e9a
harshadac2410 <111185574+harshadac2410@users.noreply.github.com>
2024-07-13 21:33:53 +0530

    operations

---
+++ Operations.js
@@ -0,0 +1,28 @@
+let value = 3
+let negValue = -value //gives negative value
+console.log(negValue); //-3
+
+console.log(2*2); //basics
+
+let str1 = "Hello"
+let str2 = " Harsha"
+
+let str3 = str1 + str2
+console.log(str3); //Hello Harsha
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2) ; //122
+console.log(1 + 2 + "2"); //32
+
+console.log(true);//true
+console.log(+true); //1 
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);//101 
+
